2d284609b3bd9306df49e805904957b98c91ab05
Arun Mohan <31436375+arunm8489@users.noreply.github.com>
2017-11-26 11:39:32 +0530

    Add files via upload

---
+++ Powershell-resizing VM.ps1
@@ -0,0 +1,13 @@
+﻿#resizing vm
+$rgname="name of resource group"
+$vmname="name of vm"
+
+
+
+
+
+Stop-AzureRmVM -ResourceGroupName $rgname -Name $vmname -Force
+$vm = Get-AzureRmVM -ResourceGroupName $rgname  -VMName $vmname
+$vm.HardwareProfile.VmSize = "Standard_F4s"
+Update-AzureRmVM -VM $vm -ResourceGroupName $rgname
+Start-AzureRmVM -ResourceGroupName $rgname  -Name $vm.name
